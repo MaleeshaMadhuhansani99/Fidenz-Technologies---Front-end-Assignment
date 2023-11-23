95c2c28015dcf22fc3af136eb2f4210866785b12
Maleesha Manage <87364623+MaleeshaMadhuhansani99@users.noreply.github.com>
2023-11-23 22:27:42 +0530

    convert scss to css

--- weatherapp/src/Components/TopRow.js
+++ weatherapp/src/Components/TopRow.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import clouds from '../imgs/fewclouds.png'
+import './weatherapp.css'
 
-function TopRow({desiredCity, finalFormattedDate}) {
+function TopRow({desiredCity, finalFormattedDate, isSelected}) {
   return (
-    <table className="top">
+    <table className={`top ${isSelected ? 'selected' : ''}`}>
       <tbody>
         <tr>
-          <td className="left_column">
-            <h2 className="location2">{desiredCity.name}</h2>
-            <p className="date2">{finalFormattedDate} </p>
-            <img src={clouds} alt="clouds" />
-            <p>Few Clouds</p>
+          <td className={`left_column ${isSelected ? 'selected' : ''}`}>
+            <h2 className={`location2 ${isSelected ? 'selected' : ''}`}>{desiredCity.name}</h2>
+            <p className={`date2 ${isSelected ? 'selected' : ''}`}>{finalFormattedDate} </p>
+            <img className= {`cloud ${isSelected ? 'selected' : ''}`} src={clouds} alt="clouds" />
+            <p className={`cloudp ${isSelected ? 'selected' : ''}`}>Few Clouds</p>
           </td>
-          <td className="right_column">
+          <td className={`right_column ${isSelected ? 'selected' : ''}`}>
             <strong>
-              <p className="temp">
+              <p className={`temp ${isSelected ? 'selected' : ''}`}>
                 {desiredCity.main ? desiredCity.main.temp : ''}°C
               </p>
             </strong>
-            <p>
+            <p className={`tempp ${isSelected ? 'selected' : ''}`}>
               Temp Min: {desiredCity.main ? desiredCity.main.temp_min : ''}°C
             </p>
-            <p>
+            <p className={`tempp ${isSelected ? 'selected' : ''}`}>
               Temp Max: {desiredCity.main ? desiredCity.main.temp_max : ''}°C
             </p>
           </td>
